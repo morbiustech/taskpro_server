962291d86392033134820fb36b6e45eb5a7306f7
morbiustech <mithilesh@morbius.co>
2020-12-20 14:55:05 +0530

    added cors headers to server

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,10 +2,11 @@ const controller = require('../controllers/users.controller')
 
 module.exports = function(app) {
     app.use(function(req, res, next) {
-      res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-      ); 
+
+      res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      res.setHeader('Access-Control-Allow-Credentials', true);
       next();
     });
   
