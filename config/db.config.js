5e3696c6e313f849f43604e0d859304f721394f2
morbiustech <mithilesh@morbius.co>
2020-12-20 13:59:24 +0530

    db config changed to producion

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 module.exports = {
 
-    HOST : 'localhost',
-    USER : 'root',
-    PASSWORD :'secretpassword',
-    DB : 'taskpro_local',
+    HOST :process.env.HOST,
+    USER :process.env.USER,
+    PASSWORD :process.env.PASSWORD,
+    DB : process.env.DB,
     dialect : "mysql",
     pool : {
 
