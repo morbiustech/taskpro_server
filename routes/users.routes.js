729846ea0cd7649ba384b4ae701a03affdc5ed6f
morbiustech <mithilesh@morbius.co>
2021-01-01 16:39:26 +0530

    front end endpoints changed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/users.controller')
 module.exports = function(app) {
     app.use(function(req, res, next) {
 
-      res.setHeader('Access-Control-Allow-Origin','https://5fe2cb1b6bbf67a30dfd2833--taskpro.netlify.app');
+      res.setHeader('Access-Control-Allow-Origin','https://5fef01a149113d7aca4b75cc--taskpro.netlify.app');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
       res.setHeader('Access-Control-Allow-Credentials', true);
